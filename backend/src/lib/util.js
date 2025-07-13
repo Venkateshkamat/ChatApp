25d5888c059fd36a051ec9ddc34a245f1f370c79
Venkatesh Kamat <venkatesh.kamat07@gmail.com>
2025-07-13 14:41:11 -0500

    Add auth routes

--- backend/src/lib/util.js
+++ backend/src/lib/util.js
@@ -10,7 +10,7 @@ export const generateToken = (userId,res) =>{
     res.cookie("jwt",token,{
         maxAge: 7*24*60*60*1000,
         httpOnly:true, //prevents XSS attacks
-        sameSite:"strict", //CSRF attacks cross site requrest forgery attacks
+        sameSite:"strict", //CSRF attacks cross site request forgery attacks
         secure: process.env.NODE_ENV != "development"
     })
     return token;
