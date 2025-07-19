a4aac66c7cb621935a2ae87dd1dda97ba36786eb
Venkatesh Kamat <venkatesh.kamat07@gmail.com>
2025-07-18 23:48:18 -0500

    Fix deployment issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,16 +18,18 @@ const __dirname = path.resolve();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [process.env.CLIENT_URL,"http://localhost:5173"],
     credentials: true
 }))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
-if (process.env.NODE_ENV == "production") {
+
+
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("*", (req, res) => {
+    app.get("/*wildcard", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
